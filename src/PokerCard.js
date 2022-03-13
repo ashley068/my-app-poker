150904cc00c80a52b2e282daf2a3ca6161e40438
ashley68 <ashley0608ly@gmail.com>
2022-03-12 20:53:49 -0500

    adding image

--- src/PokerCard.js
+++ src/PokerCard.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./poker.css";
+import queen from "./img/poker-queen.png";
+import jack from "./img/poker-jack.png";
+import king from "./img/poker-king.png";
+
+const rankStrValid = ["A", "J", "K", "Q"];
+const suitImg = {
+  J: jack,
+  Q: queen,
+  K: king,
+};
 
 const PokerCard = (props) => {
+  const imageDisplay = (rank, type, suit) => {
+    if (rankStrValid.includes(rank))
+      return <img className="suit-avatar" src={suitImg[rank]} alt="" />;
+    else return <div className={`midPic ${type}`}>{suit}</div>;
+  };
+  console.log(props.rank, typeof props.rank);
   return (
     <div className="grid-item">
       <div className={`top-left ${props.type}`}>
@@ -12,14 +28,16 @@ const PokerCard = (props) => {
       <div></div>
       <div></div>
       <div></div>
-      {/* <div>
-        <img alt="" src="../public/poker-jack.png" />
-      </div> */}
-      <div className={`midPic ${props.type}`}>{props.suit}</div>
+      {imageDisplay(props.rank, props.type, props.suit)}
+      <div></div>
+      <div></div>
+      <div></div>
+      <div className={`bottom-right ${props.type}`}>
+        {props.rank}
+        <br />
+        <div>{props.suit}</div>
+      </div>
     </div>
-    // <div>
-    //   <img src="../public/poker-king.png" alt="" />
-    // </div>
   );
 };
 
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 class SearchBar extends React.Component {
   state = { rank: "", suit: "" };
 
@@ -18,18 +19,22 @@ class SearchBar extends React.Component {
               onChange={(e) => this.setState({ rank: e.target.value })}
             />
           </div>
+          <select
+            className="ui dropdown"
+            value={this.state.suit}
+            onChange={(e) => this.setState({ suit: e.target.value })}
+          >
+            <option value="">Suit</option>
+            <option value="clubs">♣ clubs</option>
+            <option value="diams">♦ diams</option>
+            <option value="hearts">♥ hearts</option>
+            <option value="spades">♠ spades</option>
+          </select>
+          <br />
+          <button className="ui button search-btn" type="submit">
+            Search
+          </button>
         </form>
-        <select
-          className="ui dropdown"
-          value={this.state.suit}
-          onChange={(e) => this.setState({ suit: e.target.value })}
-        >
-          <option value="">Suit</option>
-          <option value="clubs">♣ clubs</option>
-          <option value="diams">♦ diams</option>
-          <option value="hearts">♥ hearts</option>
-          <option value="spades">♠ spades</option>
-        </select>
       </div>
     );
   }
